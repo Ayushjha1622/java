1bae29cb90eb697638cda14751a0094d3c2bc952
Ayush jha <119880444+Ayushjha1622@users.noreply.github.com>
2024-04-20 13:00:12 +0530

    Add files via upload

---
+++ pr_14_01.java
@@ -0,0 +1,9 @@
+public class pr_14_01 {
+    public static void main(String[] args) {
+        // int a=7 syntax error
+        // int age=56;
+        // int year-born=2000-56; logical error
+
+        System.out.println(6 / 0);
+    }
+}
